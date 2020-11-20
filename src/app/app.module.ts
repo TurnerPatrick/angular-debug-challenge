30770382d99fba6ac17a1b0899d389730b73f468
TurnerPatrick <pturner31@gmail.com>
2020-11-20 13:06:59 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// Import for the shared module and ALL of its components
+// Import for the shared module and all of its components
 import { SharedModule } from './shared/shared.module';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+
+// Import the Forms Module that comes with Angular
+import{ FormsModule } from '@angular/forms';
 import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
 import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/budget-item-card.component';
-import { AddItemFormComponent } from './add-item-form/add-item-form.component';
-import { FormsModule } from '@angular/forms';
 import { EditItemModalComponent } from './edit-item-modal/edit-item-modal.component';
 
 @NgModule({
@@ -20,17 +22,19 @@ import { EditItemModalComponent } from './edit-item-modal/edit-item-modal.compon
     AppComponent,
     MainPageComponent,
     LoginComponent,
+    AddItemFormComponent,
     BudgetItemListComponent,
     BudgetItemCardComponent,
-    AddItemFormComponent,
     EditItemModalComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/edit-item-modal/edit-item-modal.component.ts
+++ src/app/edit-item-modal/edit-item-modal.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input, Inject } from '@angular/core';
 import { BudgetItem } from '../shared/budget-item';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-
 @Component({
   selector: 'app-edit-item-modal',
   templateUrl: './edit-item-modal.component.html',
@@ -10,10 +9,10 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class EditItemModalComponent implements OnInit {
 
-  constructor(public dialogRef:MatDialogRef<EditItemModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public item: BudgetItem) { }
+  constructor(public dialogRef: MatDialogRef<EditItemModalComponent>,
+    @Inject(MAT_DIALOG_DATA)public item: BudgetItem) { }
 
-    //onSubmitted method which handles new updates
+    //OnSubmitted method which handles new updates
     onSubmitted(updatedItem:BudgetItem){
       this.dialogRef.close(updatedItem)
     }
